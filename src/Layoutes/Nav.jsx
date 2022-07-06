92970d91ca4b07d53ea10bd0dfa85303c0aab6ee
Maciej Rożnawski <intro202@wp.pl>
2022-07-06 12:25:30 +0200

    add card

--- src/Layoutes/Nav.jsx
+++ src/Layoutes/Nav.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import '../Styles/Menu/BoxLevel.css';
 
 const NavBar = () => {
+
     return ( 
-        <div>
-              <Link to="/LevelOne">
-                    <li><a>LevelOne</a></li>
-              </Link>
-              <Link to="/LevelTwo">
-                    <li><a>LevelTwo</a></li>
-              </Link>
-        </div>
+            <div className="MainBox">
+                  <div class="container">
+                        <div class="row">
+                              <div class="col">
+                                    <div className="card-level">
+                                          <Link to="/LevelTwo">
+                                                <div className="Button">
+                                                      <a>Start</a>
+                                                </div>
+                                          </Link>
+                                    </div>
+                              </div>
+                              <div class="col">
+                                    <div className="card-level">
+                                          <Link to="/LevelTwo">
+                                                <div className="Button">
+                                                      <a>Start</a>
+                                                </div>
+                                          </Link>
+                                    </div>
+                              </div>
+                        </div>
+                  </div>
+            </div>
      );
 }
  
--- src/Page/StartLevel.jsx
+++ src/Page/StartLevel.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import BackGroundAnimation from "../Layoutes/Background";
 import '../Styles/Menu/StartLevel.css'
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const StartLevel = () => {
 
